b36324b06660d98e8c74d2340396f3062e265928
eldorjon88 <eldorjonturamurodov2@gmail.com>
2025-10-14 19:55:10 +0500

    create __init__.py

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -5,6 +5,3 @@ from .anime_seria import router as anime_serial_router
 from .serial import router as serial_router
 from .seral_seria import router as serial_serial_router
 from .mix import router as mix_router
-
-
-
