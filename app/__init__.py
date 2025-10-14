f887ec56cfe650c7a68760d16ac47160028411c6
eldorjon88 <eldorjonturamurodov2@gmail.com>
2025-10-14 19:49:17 +0500

    create __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,7 @@
+from .movies import router as movies_router
+from .ganres import router as ganres_router
+from .anime import router as animes_router
+from .anime_seria import router as anime_serial_router
+from .serial import router as serial_router
+from .seral_seria import router as serial_serial_router
+from .mix import router as mix_router
